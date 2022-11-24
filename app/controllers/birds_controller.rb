0e987beacdaf3c5ef1e0c455cac85959d4447f0e
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-11-25 02:18:14 +0300

    complete

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,16 +1,24 @@
 class BirdsController < ApplicationController
 
+  # POST /birds using strong params 
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # Other controller actions below
+
   # GET /birds
   def index
     birds = Bird.all
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
+  # # POST /birds
+  # def create
+  #   bird = Bird.create(name: params[:name], species: params[:species])
+  #   render json: bird, status: :created
+  # end
 
   # GET /birds/:id
   def show
@@ -20,6 +28,13 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+  
+  # private methods
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
